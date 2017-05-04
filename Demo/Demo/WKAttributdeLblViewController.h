add5639021b6c9c2abea559ef40d1b6ce634d9eb
wyxlh <wangyu@wangyudeMacBook-Pro.local>
2017-05-04 19:12:20 +0800

    增加富文本编辑

---
+++ Demo/Demo/WKAttributdeLblViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKAttributdeLblViewController.h
+//  Demo
+//
+//  Created by 王宇 on 2017/5/4.
+//  Copyright © 2017年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKAttributdeLblViewController : UIViewController
+
+@end
---
+++ Demo/Demo/WKAttributdeLblViewController.m
@@ -0,0 +1,61 @@
+//
+//  WKAttributdeLblViewController.m
+//  Demo
+//
+//  Created by 王宇 on 2017/5/4.
+//  Copyright © 2017年 wy. All rights reserved.
+//
+
+#import "WKAttributdeLblViewController.h"
+#import "AttributedLbl.h"
+#import "UILabel+YBAttributeTextTapAction.h"
+#define WKAlertShow(messageText,buttonName) \
+UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:(messageText) \
+delegate:nil cancelButtonTitle:(buttonName) otherButtonTitles: nil];\
+[alert show];
+@interface WKAttributdeLblViewController ()<YBAttributeTapActionDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *oneLbl;
+@property (weak, nonatomic) IBOutlet UILabel *twoLbl;
+@property (weak, nonatomic) IBOutlet UILabel *threeLbl;
+@property (weak, nonatomic) IBOutlet UILabel *fourLbl;
+@property (weak, nonatomic) IBOutlet UILabel *fiveLbl;
+@property (weak, nonatomic) IBOutlet UILabel *sixLbl;
+@end
+
+@implementation WKAttributdeLblViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *oneStr=@"合计: ¥100.00";
+    [AttributedLbl setRichText:self.oneLbl titleString:oneStr textFont:[UIFont systemFontOfSize:25] fontRang:NSMakeRange(4, oneStr.length-4) textColor:[UIColor redColor] colorRang:NSMakeRange(4, oneStr.length-4)];
+    
+    NSString *twoStr=@"今天我花了¥100.00元 在路上捡了¥50.00元";
+    [AttributedLbl setRichTwoText:self.twoLbl titleString:twoStr textFirstFont:[UIFont systemFontOfSize:22] fontFirstRang:NSMakeRange(5, 7) textFirstColor:[UIColor redColor] colorFirstRang:NSMakeRange(5, 7) textSecondFont:[UIFont systemFontOfSize:22] fontSecondRang:NSMakeRange(19, 6) textSecondColor:[UIColor redColor] colorSecondRang:NSMakeRange(19, 6)];
+    
+    NSString *threeStr=@"原价: ¥100.00";
+    [AttributedLbl setRichTextTwoTypsFontAndColor:self.threeLbl titleString:threeStr textColor:[UIColor redColor] colorRang:NSMakeRange(4, threeStr.length-4) textFirstFont:[UIFont systemFontOfSize:17] fontFirstRang:NSMakeRange(4, 1) textSecondFont:[UIFont systemFontOfSize:35] fontSecondRang:NSMakeRange(5, threeStr.length-5)];
+    
+    NSString *fourStr=@"原价: ¥100.00";
+    [AttributedLbl setRichTextOnlyColor:self.fourLbl titleString:fourStr textColor:[UIColor redColor] colorRang:NSMakeRange(4, fourStr.length-4)];
+    
+    
+    NSString *sieStr=@"来呀,点击我呀";
+    [AttributedLbl setRichTextOnlyColor:self.sixLbl titleString:sieStr textColor:[UIColor redColor] colorRang:NSMakeRange(3, sieStr.length-3)];
+    //这个是借鉴的别人的
+    [_sixLbl yb_addAttributeTapActionWithStrings:@[@"点击我呀"] delegate:self];
+    
+}
+//delegate
+- (void)yb_attributeTapReturnString:(NSString *)string range:(NSRange)range index:(NSInteger)index
+{
+    NSString *message = [NSString stringWithFormat:@"点击了“%@”字符\nrange: %@\nindex: %ld",string,NSStringFromRange(range),index];
+    WKAlertShow(message, @"取消");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
